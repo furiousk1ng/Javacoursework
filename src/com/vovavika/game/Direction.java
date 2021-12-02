2c4e8dc896a01ff1ce329a13d94827d69c639036
Filinov Vladimir <fbvbnc@gmail.com>
2021-12-02 19:53:39 +0300

    2048 without menu

---
+++ src/com/vovavika/game/Direction.java
@@ -0,0 +1,8 @@
+package com.vovavika.game;
+
+public enum Direction {
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN
+}
---
+++ src/com/vovavika/game/Point.java
@@ -0,0 +1,14 @@
+package com.vovavika.game;
+
+public class Point {
+
+    public int row;
+    public int col;
+
+    public Point(int row, int col)
+    {
+        this.row = row;
+        this.col = col;
+
+    }
+}
